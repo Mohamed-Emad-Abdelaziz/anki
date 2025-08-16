d091aa2924128c2929b3bd509f7a4f18f963cb03
Mohamed <mmm3emad@gmail.com>
2025-08-16 11:31:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* config.js — put your own Web App URL here */
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx0gr49ZOuiRO-oLqp_a1vw34XwzY866yC65aDxDwL_xvw4wE0QlpqQiIrYjNVuJosD/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzHsLtWBROzaPZM3SPzHJW3k_gchsK-_v-mA3jp_lz0FMYrfGgoyXcdJoNAXTiCXvJs/exec';
